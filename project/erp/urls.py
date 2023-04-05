6494cc66ecc7fe8e94f7f695c2c9e2cbf1393b12
JHectorZ <jesushellerque@gmail.com>
2023-04-04 21:06:29 -0600

    Se ha mejorado la view de index, se le ha agregado el menu y su template

--- project/erp/urls.py
+++ project/erp/urls.py
@@ -1,6 +1,13 @@
 from . import views
 from django.urls import path
 
+
+
+app_name = 'erp'
+
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('show', views.show, name = 'show'),
+    path('register', views.register, name = 'register')
 ]
+
--- project/erp/views.py
+++ project/erp/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.http import HttpResponse
+from .models import Departament, Employee, Job
 
 
 
 def index(request):
-    return HttpResponse("Hello world!")
-    
+    departament = Departament.objects.count()
+    return render(request, 'erp/index.html', {'departament': departament})
+
+
+def show(request):
+    return HttpResponse("Estas en show")
+
+
+def register(request):
+    return HttpResponse("Estas en registro")
+
 
